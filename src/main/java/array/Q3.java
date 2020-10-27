e3399e5c61d050a28b2d92be243989b3a46be055
HB <hbheyhoo@163.com>
2020-10-27 15:44:46 +0800

    剑指offer面试题10: 求斐波拉契数列的第n项

--- src/main/java/array/Q3.java
+++ src/main/java/array/Q3.java
@@ -1,90 +1,90 @@
 package array;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @Author: HB
- * @Description: 面试题03OII - 不修改数组找出重复的数字
- *               描述: 在一个长度为 n + 1的数组 nums 里的所有数字都在 0～n 的范围内。所以数组中至少有一个数字是重复的。
- *               请找出数组中任意一个重复的数字, 但不能修改输入的数组。
+ * @Description: 面试题03 - 数组中重复的数字
+ *               描述: 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，
+ *               但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。 2 <= n <= 100000
  *               Case:
- *                   Input: [2,3,5,4,3,2,6,7]
+ *                   Input: [2,3,1,0,2,5,3]
  *                   Output: 2/3
- * @CreateDate: 15:15 2020/10/15
+ * @CreateDate: 15:15 2020/10/14下·
  */
 
 public class Q3 {
 
     /**
      * @Author: HB
-     * @Description: 数组复制
+     * @Description: 哈希表解法
      *               时间复杂度：O(N)
-     *               空间复杂度：O(1)
-     * @Date: 15:17 2020/10/15
+     *               空间复杂度：O(N)
+     * @Date: 15:17 2020/10/14
      * @Params: null
      * @Returns:
     */
-    public static int findRepeatNumberByCopy(int[] nums) {
-        // 开辟一个辅助数字
-        int[] temp = new int[nums.length];
-        for (Integer value : nums) {
-            if (value == temp[value]) {
-                return value;
+    public int findRepeatNumberByHashTable(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            // 若插入失败, 则代表哈希表中已经右重复元素, 则直接返回
+            if (!set.add(nums[i])){
+                return nums[i];
             }
-            temp[value] = value;
         }
         return 0;
     }
 
     /**
      * @Author: HB
-     * @Description: 基于二分查找的解法 - 迭代解法
+     * @Description: 排序 + 双指针
      *               时间复杂度：O(NlogN)
      *               空间复杂度：O(1)
-     * @Date: 15:19 2020/10/15
-     * @Params:
+     * @Date: 15:18 2020/10/14
+     * @Params: null
      * @Returns:
     */
-    public static int findRepeatNumberByBinarySearch(int[] nums) {
-        int start = 1;
-        int end = nums.length - 1;
-        int middle = 0;
-        while (start <= end) {
-            // 不直接使用 (end + start) / 2 是为了防止int类型的溢出
-            middle = (end - start) / 2 + start;
-            // 计算出现次数
-            int count = countRange (nums, start, middle);
-            if (end == start) {
-                if (count > 1) {
-                    return start;
-                }else {
-                    break;
-                }
-            }
-            // 若 [start,middle]范围的数字在数组中出现的次数 大于 (middle - start) + 1, 则代表重复
-            // 数字出现在[start,middle]中, 否则重复数字出现在[middle+1, end]中
-            if (count > (middle - start + 1)){
-                end = middle;
-            } else {
-                start = middle + 1;
-            }
+    public int findRepeatNumberBySort(int[] nums) {
+        // 先对数组进行排序 - 使用快排排序进行
+        // sort(nums);
+        Arrays.sort(nums);
+        // 对于已排序的元素进行遍历, 找到相同元素
+        for (int i = 0; i < nums.length - 1; i++) {
+            int j = i + 1;
+            if (nums[i] == nums[j])
+                return nums[i];
         }
         return 0;
     }
 
-    // 统计start 至 middle 在数组出出现的
-    private static int countRange (int[] nums, int start, int middle) {
-        int count = 0;
+    /**
+     * @Author: HB
+     * @Description: 原地解法
+     *               时间复杂度：O(N)
+     *               空间复杂度：O(1)
+     * @Date: 15:19 2020/10/14
+     * @Params:
+     * @Returns:
+    */
+    public int findRepeatNumbeByInPlacer(int[] nums) {
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= start && nums[i] <= middle) {
-                count++;
+            // 若nums[i] 不等于i, 则代表nums[i]不在正确位置上
+            while (nums[i] != i) {
+                // 将nums[i] 与 nums[nums[i]]比较, 若相等, 则代表找到了重复的元素, 否则进行交换
+                if(nums[i] == nums[nums[i]]){
+                    return nums[i];
+                } else {
+                    // 将nums[i] 放在属于他的位置上
+                    int temp = nums[nums[i]];
+                    nums[nums[i]] = nums[i];
+                    nums[i] = temp;
+                }
             }
         }
-        return count;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {3,3,5,3,1,2,6,7};
-        System.out.println(findRepeatNumberByCopy(nums));
-        System.out.println(findRepeatNumberByBinarySearch(nums));
+        return 0;
     }
 }
