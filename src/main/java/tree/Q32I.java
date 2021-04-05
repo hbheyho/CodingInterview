ff2f26704cd19aeedaeb6f934b896cbc5eb73171
HB <hbheyhoo@163.com>
2021-04-05 19:02:06 +0800

    剑指offer面试题22：链表中倒数第k个节点 剑指offer面试题31：栈的压入和弹出序列 剑指offer面试题32I：从上到下打印二叉树 剑指offer面试题32II：从上到下打印二叉树II 剑指offer面试题32III：从上到下打印二叉树III

---
+++ src/main/java/tree/Q32I.java
@@ -0,0 +1,62 @@
+package tree;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: HB
+ * @Description: 面试题32I - 从上到下打印二叉树
+ *               描述: 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，
+ *               那么它是对称的。
+ *               Case:
+ *                   Input:     1
+ *                            /   \
+ *                           2     2
+ *                          / \   / \
+ *                         8  10 10  8
+ *                   OutPut: [1,2,2,8,10,10,8]
+ *               Limit: 0 <= 节点个数 <= 1000
+ *               Remark:
+ * @CreateDate: 13:01 2021/4/5
+ */
+
+public class Q32I {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * @Author: HB
+     * @Description: 层次遍历解法
+     * @Date: 17:55 2021/4/5
+     * @Params: null
+     * @Returns:
+    */
+    public int[] levelOrder(TreeNode root) {
+        if (root == null)
+            return new int[0];
+        List<Integer> result = new ArrayList<>();
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.val);
+            if (node.left != null)
+                queue.offer(node.left);
+            if (node.right != null)
+                queue.offer(node.right);
+        }
+
+        return result.stream().mapToInt(Integer::valueOf).toArray();
+    }
+}
---
+++ src/main/java/tree/Q32III.java
@@ -0,0 +1,102 @@
+package tree;
+
+import java.util.*;
+
+/**
+ * @Author: HB
+ * @Description: 面试题32III - 从上到下打印二叉树III
+ *               描述:  请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，
+ *               第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ *               Case:
+ *                   Input:     3
+ *                            /   \
+ *                           9     10
+ *                                / \
+ *                               8  7
+ *                   OutPut: [[1],[10,9],[8,7]
+ *               Limit: 0 <= 节点个数 <= 1000
+ *               Remark:
+ * @CreateDate: 18:37 2021/4/5
+ */
+
+public class Q32III {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public  TreeNode (int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            // 双端队列
+            LinkedList<Integer> subResult = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int level = result.size();
+                // 处于奇数层, 则从后到前存储
+                // 处于偶数层, 则从前到后存储
+                if (level % 2 != 0)
+                    subResult.addFirst(node.val);
+                else
+                    subResult.addLast(node.val);
+
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            result.add(subResult);
+        }
+
+        return result;
+
+    }
+
+    /**
+     * @Author: HB
+     * @Description: reverse API解法
+     * @Date: 18:52 2021/4/5
+     * @Params: null
+     * @Returns:
+    */
+    public List<List<Integer>> levelOrderByAPI(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> subResult = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                subResult.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            if (result.size() % 2 != 0) {
+                Collections.reverse(subResult);
+            }
+
+            result.add(subResult);
+        }
+
+        return result;
+
+    }
+}
